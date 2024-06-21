8dd89aa763cf878133e9b9551dac0080e7b9a6a0
Emka <slolzguru123@gmail.com>
2024-06-21 18:50:34 +0600

    Updated App with styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
 import CountryList from './CountryList';
 import CountryDetail from './CountryDetail';
+import './App.css';
 
 const App: React.FC = () => {
   const [selectedCountry, setSelectedCountry] = useState<string | null>(null);
 
   return (
     <div className="App">
-      <CountryList setSelectedCountry={setSelectedCountry} />
-      {selectedCountry ? (
-        <CountryDetail countryName={selectedCountry} />
-      ) : (
-        <div>Please select a country</div>
-      )}
+      <div className="CountryList">
+        <CountryList setSelectedCountry={setSelectedCountry} />
+      </div>
+      <div className="CountryDetail">
+        {selectedCountry ? (
+          <CountryDetail countryName={selectedCountry} />
+        ) : (
+          <div>Please select a country</div>
+        )}
+      </div>
     </div>
   );
 };
